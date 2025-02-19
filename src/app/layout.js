2cf432d9e718b9062eecb89f137f2b04054b3006
briannjara <briannjaramba138@gmail.com>
2025-02-19 17:03:30 +0300

    first-commit

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: {
+    default: "Njaramba Portfolio",
+  },
+  description:
+    "A unique creative portfolio designed by CodeBucks with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout CodeBucks on youtube.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={clsx(
+          inter.variable,
+          "bg-background text-foreground font-inter"
+        )}
+      >
+        {children}
+
+        <div id="my-modal" />
+      </body>
+    </html>
+  );
+}
